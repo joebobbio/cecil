7c05a3ddfdbdb947c100b1c9b77d7f328f5f02fe
phene <60532517+joebobbio@users.noreply.github.com>
2021-05-06 02:37:24 -0400

    Update disconnect.js

--- src/commands/music/disconnect.js
+++ src/commands/music/disconnect.js
@@ -6,16 +6,16 @@ module.exports =
             super(...args, {
                 name: "leave",
                 type: "music",
-                description: "Tell the bot to disconnect from the voice channel.",
+                description: "Leave the current voice channel",
                 usage: "No arguments required.",
-                aliases: ["dc", "disconnect", "stop"],
-                saying: "Stop disconnecting me, its annoying.",
+                aliases: ["dc", "disconnect", "stop", "fuckoff"],
+                saying: "Do you want me in here, or not?",
                 cooldown: 2
             });
         }
 
         main(msg) {
-            const { connection } = msg.guild.voice;
+            const { connection } = msg.guild.voice
 
             connection.disconnect();
             this.musicQueues.delete(msg.guild.id);
