2d83e6bcf991a89a3ec9d80c0bc445b095a3f31c
phene <60532517+joebobbio@users.noreply.github.com>
2021-05-06 02:34:58 -0400

    Create permissions.js

---
+++ src/commands/dev/permissions.js
@@ -0,0 +1,26 @@
+const { Command } = require("../../../lib");
+
+module.exports =
+    class extends Command {
+        constructor(...args) {
+            super(...args, {
+                name: "permissions",
+                type: "dev",
+                description: "Check user permissions",
+                usage: "No arguments",
+                aliases: ["perms"],
+                saying: null,
+                cooldown: 5
+            });
+        }
+
+        toFriendly(string) {
+               return string.split("_").map(s => `${s[0].toUpperCase()}${s.slice(1)}`).join(" ").trim()
+        }
+
+        main(msg) {
+               const perms = msg.member.permissions.serialize();
+               msg.send(`**Permissions for <@!${msg.member.id}> in ${msg.guild.name}**`)
+               msg.send(Object.entries(perms).map(p => `${this.toFriendly(p[0])}: ${p[1]}\n`))
+        }
+    }
