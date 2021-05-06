32d9b9df4f42c5a03eda8b652630b006ead92fc7
phene <60532517+joebobbio@users.noreply.github.com>
2021-05-06 02:11:21 -0400

    holy shit

--- config/index.js
+++ config/index.js
@@ -1,17 +1,14 @@
 module.exports = {
-    prefix: "+",
+    prefix: "c!",
     version: require("../package.json").version,
-    owners: new Set(["329331044828446722", "745058406083198994"]), 
-    blacklisted: new Set([""]),
-    serverInvite: "https://discord.gg/2RKPmDg2A6",
+    owners: new Set(["347791894115385355", "813131596835717150"]),
+    blacklisted: new Set(),
+    serverInvite: "https://discord.gg/NSGmteyrmg",
     defaultChannels: {
-        suggestions: "758005843127173221",
-        bugs: "811284407134322700"
+        suggestions: "837850408533360670",
+        bugs: "837850427105214484"
     },
     todo:
         [
-            "User-based Marketplace!",
-            "Prestiging!",
-            "Random events for more stars"
         ]
 };
