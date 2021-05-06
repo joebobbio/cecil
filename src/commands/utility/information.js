bd1584bf9776f770120b4b9b314902bb38e8091a
phene <60532517+joebobbio@users.noreply.github.com>
2021-05-06 02:44:45 -0400

    Update information.js

--- src/commands/utility/information.js
+++ src/commands/utility/information.js
@@ -4,11 +4,11 @@ module.exports =
     class extends Command {
         constructor(...args) {
             super(...args, {
-                name: "information",
+                name: "info",
                 type: "utility",
                 description: "Basic info about the bot.",
                 usage: "No arguments required.",
-                aliases: ["i", "info"],
+                aliases: ["i"],
                 saying: "It's not like the info is interesting.",
                 cooldown: 2
             });
@@ -36,12 +36,12 @@ module.exports =
                     { name: "Version", value: this.client.config.version },
                     { name: "Uptime", value: `${Math.floor(uptime)} ${unit}` },
                     { name: "Todo List", value: this.client.config.todo.join("\n") },
-                    { name: "Recent Update", value: "Database corruption lead to data wipe, actively working on solution to fix data, +meme command added." },
-                    { name: "Credits", value: "Made by Kinglalu,DAONE, and LinuxTerm\nGraphics/Emojis by Goobermeister\nBug hunters: Mikelime & CompactCow\nOriginally by Horsey4 and Airplane Bong." },
-                    { name: "Supporters", value: `${msg.prefix}support for more info!` },
+                    { name: "Recent Update", value: "None" },
+                    { name: "Credits", value: "By [Phene](mailto:phene@phene.dev) & DAONE.\nBased on [Mr. Grape](https://github.com/kinglalu/Mr.Grape)." },
                     { name: "Number of servers", value: this.client.guilds.cache.size },
                     { name: "Number of Users", value: this.client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString() }
                 );
             msg.send(info);
         }
+
     };
