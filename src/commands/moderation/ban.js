07d3565c2716532bcc06d16599c11badb1841c40
phene <60532517+joebobbio@users.noreply.github.com>
2021-06-18 02:40:49 -0400

    Update ban.js

--- src/commands/moderation/ban.js
+++ src/commands/moderation/ban.js
@@ -8,30 +8,30 @@ module.exports =
                 type: "moderation",
                 aliases: ["hammer"],
                 description: "Ban people.",
-                usage: "<mention|userID> <reason>",
+                usage: "<mention|user ID> [reason]",
                 cooldown: 2,
-                saying: "Don't spam this command.",
+                saying: "Hammer discipline!",
                 requiredPermissions: ["BAN_MEMBERS"]
             });
         }
 
         async main(msg) {
-            if (!msg.params[0]) return msg.send("Who should I ban?");
+            if (!msg.params[0]) return msg.send("I can't ban anyone if you don't tell me who to ban.");
 
             const target = msg.mentions.members.first() ||
                 await msg.guild.members.fetch(msg.params[0]).catch(() => { null; });
 
             if (!target) return msg.send("Give me a valid user that I can ban!");
-            else if (target.id === msg.author.id) return msg.send("Imagine banning urself");
-            else if (target.id === this.client.user.id) return msg.send("Woah there, I'm too cool for the hammer");
+            else if (target.id === msg.author.id) return msg.send("You can't call that on yourself.");
+            else if (target.id === this.client.user.id) return msg.send("Why me?");
             else if (!target.bannable) return msg.send("That isn't a bannable user!");
 
             const reason = msg.params.slice(1).join(" ") || "No reason given";
 
             target.ban({ reason: reason });
 
-            msg.send(`:hammer: ${target.user.username} has been banned with an iron fist.`);
+            msg.send(`${msg.emojis.done} ${target.user.username} has been banned from **${msg.guild.name}**.`);
 
-            target.send(`You were banned from \`${msg.guild.name}\` for \`${reason}\`.`).catch(() => null);
+            target.send(``).catch(() => null);
         }
     };
