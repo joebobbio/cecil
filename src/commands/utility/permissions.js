4a83ed999591a4cc9f3120d93c809b63f3c44965
phene <60532517+joebobbio@users.noreply.github.com>
2021-06-18 02:47:49 -0400

    Update permissions.js

--- src/commands/utility/permissions.js
+++ src/commands/utility/permissions.js
@@ -13,7 +13,7 @@ module.exports =
                 cooldown: 5
             });
         }
-
+        
         toFriendly(string) {
                return string.split("_").map(s => `${s[0].toUpperCase()}${s.slice(1)}`).join(" ").trim()
         }
